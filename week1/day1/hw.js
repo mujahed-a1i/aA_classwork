a308e58ff3309ba594cc7c22caa5c09a2667001b
mujahed-a1i <103666005+mujahed-a1i@users.noreply.github.com>
2022-09-27 18:07:14 -0400

    finish w1d1

---
+++ week1/day1/hw.js
@@ -0,0 +1,22 @@
+console.log(4+4/2); // 6
+console.log((2 + -7) * 3); // -15
+console.log(101 % 10); // -1
+console.log(12 - 4 % 3); // 11
+console.log(true && false); // false
+console.log(true && !(false || false)); // true
+console.log(!true && !(false || false)); // false
+console.log('cat' + 'dog'); // catdog
+console.log(2 + 'pizza'); // 2pizza
+console.log(2.5 * 'fish'); // NaN
+console.log(5>=11); // false
+console.log(5 === 5.0); // true
+console.log(7 !== 7.1); // true 
+console.log(5 + 5 > 8); // true 
+console.log(6 + 6 !== 12); // false
+console.log(2 > 1 || false); // true 
+console.log('true' === true); // false
+console.log('bootcamp'.indexOf('camp')) // 4
+console.log(10 % 2 === 0); //true 
+console.log(21 % 2 === 0); // false
+console.log(21 % 2 !== 0); //true  
+console.log(21 % 2 === 1); // true
