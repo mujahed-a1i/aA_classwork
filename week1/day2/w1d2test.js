3865350f850e25704d9b24390941b03286b8934a
mujahed-a1i <103666005+mujahed-a1i@users.noreply.github.com>
2022-09-27 19:19:52 -0400

    w1d2

--- week1/day2/w1d2test.js
+++ week1/day2/w1d2test.js
@@ -2,11 +2,6 @@ function foo(){
     return "bar"
 } 
 
-function name(p1, p2){
-    return p1 + " " + p2;
-}
-console.log(name('Jon','Diaz'));
-
 let foo2 = function (){
     return 'bar';
 }
@@ -15,4 +10,25 @@ let foo3 = () => {
     return 'bar';
 }
 
-console.log(name('Mujahed',"Ali"));
+function name(p1, p2){
+    return p1 + " " + p2;
+}
+console.log(name('Jon','Diaz'));
+
+function average(num1, num2){
+    let sum = num1 + num2;
+    let avg = sum/2
+    return avg;
+}
+
+function average2(num1, numb2){
+    let sum = sumNums(num1, num2)
+    let avg = sum/2
+    return avg;
+}
+
+function sumNums(num1, num2){
+    return (num1 + num2);
+}
+
+console.log(average(14,12));
