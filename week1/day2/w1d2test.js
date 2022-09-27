2375780133374fdd576e40bcaefebc6a32c528b4
mujahed-a1i <103666005+mujahed-a1i@users.noreply.github.com>
2022-09-27 18:55:50 -0400

    w1d2

--- week1/day2/w1d2test.js
+++ week1/day2/w1d2test.js
@@ -1,6 +1,16 @@
-let num = 20;
-if(num = 30){
-    console.log("In the if");
-}else{
-    console.log("In the else");
+function foo(){
+    return "bar"
+} 
+
+function name(p1, p2){
+    return p1 + " " + p2;
+}
+console.log(name('Jon','Diaz'));
+
+let foo = function (){
+    return 'bar';
+}
+
+let foo = () => {
+    return 'bar';
 }
